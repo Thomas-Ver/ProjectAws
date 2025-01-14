37ccbd73528386aa2fccdabbe43d64d49dd294ed
Corentin MARQUET <cormarquet@gmail.com>
2025-01-14 09:50:35 +0100

    Removes SQS notification and Lambda trigger from code

--- ConsolidateWorkersLambda/src/main/java/ConsolidateWorker.java
+++ ConsolidateWorkersLambda/src/main/java/ConsolidateWorker.java
@@ -1,198 +1,198 @@
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+    import java.io.BufferedReader;
+    import java.io.ByteArrayOutputStream;
+    import java.io.IOException;
+    import java.io.InputStream;
+    import java.io.InputStreamReader;
+    import java.io.ObjectInputStream;
+    import java.io.ObjectOutputStream;
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.List;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+    import com.opencsv.CSVReader;
+    import com.opencsv.exceptions.CsvException;
 
-import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+    import software.amazon.awssdk.core.ResponseInputStream;
+    import software.amazon.awssdk.core.sync.RequestBody;
+    import software.amazon.awssdk.services.s3.S3Client;
+    import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+    import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+    import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+    import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+    import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+    import software.amazon.awssdk.services.s3.model.S3Exception;
 
-public class ConsolidateWorker {
+    public class ConsolidateWorker {
 
-    private final S3Client s3Client;
-    private final String outputBucket;
+        private final S3Client s3Client;
+        private final String outputBucket;
 
-    public ConsolidateWorker(String outputBucket) {
-        this.s3Client = S3Client.builder().build();
-        this.outputBucket = outputBucket;
-    }
-
-    public String run(GetObjectRequest request) {
-        HashMap<String, List<List<String>>> consolidateMap = new HashMap<>();
-        try {
-            try (ResponseInputStream<GetObjectResponse> s3ObjectResponse = s3Client.getObject(request)) {
-                consolidateMap = ReadConsolidateMapFromS3(outputBucket);
-
-                processCsv(s3ObjectResponse, consolidateMap);
-
-                WriteNewhashmapToS3(consolidateMap, outputBucket);
-
-                return "Successfully processed ";
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Error processing S3 object: " + e.getMessage(), e);
-        }
-    }
-
-    public void processCsv(InputStream inputStream, HashMap<String, List<List<String>>> ConsolidateMap)
-            throws IOException, CsvException {
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)); CSVReader csvReader = new CSVReader(reader)) {
-
-            List<String[]> records = csvReader.readAll();
-
-            // on va sur chaque ligne en esquivant la premiére
-            for (int i = 1; i < records.size(); i++) {
-                String[] record = records.get(i);
-                // record[0] correspond à la ligne i 1er colone (ici la date)
-                processRecord(record, ConsolidateMap);
-
-            }
-
-        }
-    }
-
-    private void processRecord(String[] record, HashMap<String, List<List<String>>> ConsolidateMap) {
-
-        String sourceIp = record[1];
-        String destIp = record[2];
-
-        if (sourceIp.isEmpty() || destIp.isEmpty()) {
-            return;
+        public ConsolidateWorker(String outputBucket) {
+            this.s3Client = S3Client.builder().build();
+            this.outputBucket = outputBucket;
         }
 
-        String key = String.format("%s,%s", sourceIp, destIp);
+        public String run(GetObjectRequest request) {
+            HashMap<String, List<List<String>>> consolidateMap = new HashMap<>();
+            try {
+                try (ResponseInputStream<GetObjectResponse> s3ObjectResponse = s3Client.getObject(request)) {
+                    consolidateMap = ReadConsolidateMapFromS3(outputBucket);
 
-        String flowDuration = record[3];
-        String forwardPackets = record[4];
-        String date = record[0];
+                    processCsv(s3ObjectResponse, consolidateMap);
 
-        List<String> Value = new ArrayList<>();
-        Value.add(date);
-        Value.add(flowDuration);
-        Value.add(forwardPackets);
+                    WriteNewhashmapToS3(consolidateMap, outputBucket);
 
-        // Si la clé n'existe pas on la crée
-        ConsolidateMap.putIfAbsent(key, new ArrayList<>());
-        // On ajoute la valeur à la clé
-        ConsolidateMap.get(key).add(Value);
-
-    }
-
-    private HashMap<String, List<List<String>>> ReadConsolidateMapFromS3(String bucketName) {
-        HashMap<String, List<List<String>>> map = null;
-
-        try {
-            // Vérifiez si le fichier existe sur S3
-            if (!fileExistsOnS3(s3Client, bucketName, "hashmap.ser")) {
-                System.out.println("Le fichier hashmap.set n'existe pas. Initialisation d'une HashMap vide.");
-                return new HashMap<>(); // Retourne une HashMap vide
+                    return "Successfully processed ";
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Error processing S3 object: " + e.getMessage(), e);
             }
-            // Créer une requête pour obtenir l'objet S3
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key("hashmap.ser")
-                    .build();
+        }
 
-            // Récupérer l'objet S3 en tant que flux d'entrée
-            ResponseInputStream<?> s3ObjectStream = s3Client.getObject(getObjectRequest);
+        public void processCsv(InputStream inputStream, HashMap<String, List<List<String>>> ConsolidateMap)
+                throws IOException, CsvException {
 
-            // Utiliser try-with-resources pour s'assurer que les flux sont fermés
-            try (InputStream inputStream = s3ObjectStream; ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)); CSVReader csvReader = new CSVReader(reader)) {
 
-                // Désérialiser l'objet
-                Object obj = objectInputStream.readObject();
-                if (obj instanceof HashMap) {
-                    // Suppression des avertissements de type
-                    @SuppressWarnings("unchecked")
-                    HashMap<String, List<List<String>>> tempMap = (HashMap<String, List<List<String>>>) obj;
-                    map = tempMap;
+                List<String[]> records = csvReader.readAll();
+
+                // on va sur chaque ligne en esquivant la premiére
+                for (int i = 1; i < records.size(); i++) {
+                    String[] record = records.get(i);
+                    // record[0] correspond à la ligne i 1er colone (ici la date)
+                    processRecord(record, ConsolidateMap);
+
+                }
+
+            }
+        }
+
+        private void processRecord(String[] record, HashMap<String, List<List<String>>> ConsolidateMap) {
+
+            String sourceIp = record[1];
+            String destIp = record[2];
+
+            if (sourceIp.isEmpty() || destIp.isEmpty()) {
+                return;
+            }
+
+            String key = String.format("%s,%s", sourceIp, destIp);
+
+            String flowDuration = record[3];
+            String forwardPackets = record[4];
+            String date = record[0];
+
+            List<String> Value = new ArrayList<>();
+            Value.add(date);
+            Value.add(flowDuration);
+            Value.add(forwardPackets);
+
+            // Si la clé n'existe pas on la crée
+            ConsolidateMap.putIfAbsent(key, new ArrayList<>());
+            // On ajoute la valeur à la clé
+            ConsolidateMap.get(key).add(Value);
+
+        }
+
+        private HashMap<String, List<List<String>>> ReadConsolidateMapFromS3(String bucketName) {
+            HashMap<String, List<List<String>>> map = null;
+
+            try {
+                // Vérifiez si le fichier existe sur S3
+                if (!fileExistsOnS3(s3Client, bucketName, "hashmap.ser")) {
+                    System.out.println("Le fichier hashmap.set n'existe pas. Initialisation d'une HashMap vide.");
+                    return new HashMap<>(); // Retourne une HashMap vide
+                }
+                // Créer une requête pour obtenir l'objet S3
+                GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key("hashmap.ser")
+                        .build();
+
+                // Récupérer l'objet S3 en tant que flux d'entrée
+                ResponseInputStream<?> s3ObjectStream = s3Client.getObject(getObjectRequest);
+
+                // Utiliser try-with-resources pour s'assurer que les flux sont fermés
+                try (InputStream inputStream = s3ObjectStream; ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+
+                    // Désérialiser l'objet
+                    Object obj = objectInputStream.readObject();
+                    if (obj instanceof HashMap) {
+                        // Suppression des avertissements de type
+                        @SuppressWarnings("unchecked")
+                        HashMap<String, List<List<String>>> tempMap = (HashMap<String, List<List<String>>>) obj;
+                        map = tempMap;
+                    } else {
+                        System.err.println("L'objet désérialisé n'est pas une HashMap.");
+                    }
+                }
+
+                System.out.println("HashMap chargée depuis S3 avec succès.");
+            } catch (S3Exception e) {
+                System.err.println("Erreur S3 : " + e.awsErrorDetails().errorMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("Erreur d'E/S lors de la lecture de l'objet S3.");
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                System.err.println("Classe non trouvée lors de la désérialisation.");
+                e.printStackTrace();
+            }
+
+            return map;
+        }
+
+        public static boolean fileExistsOnS3(S3Client s3, String bucketName, String key) {
+            try {
+                HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build();
+
+                HeadObjectResponse response = s3.headObject(headObjectRequest);
+                return response != null;
+            } catch (S3Exception e) {
+                if (e.statusCode() == 404) {
+                    return false; // Le fichier n'existe pas
                 } else {
-                    System.err.println("L'objet désérialisé n'est pas une HashMap.");
+                    System.err
+                            .println(
+                                    "Erreur lors de la vérification de l'existence du fichier : " + e.awsErrorDetails().errorMessage());
+                    throw e;
                 }
             }
-
-            System.out.println("HashMap chargée depuis S3 avec succès.");
-        } catch (S3Exception e) {
-            System.err.println("Erreur S3 : " + e.awsErrorDetails().errorMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("Erreur d'E/S lors de la lecture de l'objet S3.");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            System.err.println("Classe non trouvée lors de la désérialisation.");
-            e.printStackTrace();
         }
 
-        return map;
-    }
+        public void WriteNewhashmapToS3(HashMap<String, List<List<String>>> map, String bucketName) {
+            try {
+                // Créer un flux de sortie pour écrire l'objet sérialisé
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
 
-    public static boolean fileExistsOnS3(S3Client s3, String bucketName, String key) {
-        try {
-            HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(key)
-                    .build();
+                // Écrire l'objet sérialisé dans le flux de sortie
+                objectOutputStream.writeObject(map);
+                objectOutputStream.close();
 
-            HeadObjectResponse response = s3.headObject(headObjectRequest);
-            return response != null;
-        } catch (S3Exception e) {
-            if (e.statusCode() == 404) {
-                return false; // Le fichier n'existe pas
-            } else {
-                System.err
-                        .println(
-                                "Erreur lors de la vérification de l'existence du fichier : " + e.awsErrorDetails().errorMessage());
-                throw e;
+                // Convertir le flux de sortie en tableau de bytes
+                byte[] bytes = byteArrayOutputStream.toByteArray();
+
+                // Créer une demande de téléchargement
+                PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key("hashmap.ser")
+                        .build();
+
+                // Télécharger l'objet sérialisé sur S3
+                s3Client.putObject(putObjectRequest, RequestBody.fromBytes(bytes));
+
+                System.out.println("HashMap sérialisée téléchargée avec succès sur S3.");
+            } catch (S3Exception e) {
+                System.err.println("Erreur S3 : " + e.awsErrorDetails().errorMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("Erreur d'E/S lors de la sérialisation de l'objet.");
+                e.printStackTrace();
             }
         }
     }
-
-    public void WriteNewhashmapToS3(HashMap<String, List<List<String>>> map, String bucketName) {
-        try {
-            // Créer un flux de sortie pour écrire l'objet sérialisé
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-
-            // Écrire l'objet sérialisé dans le flux de sortie
-            objectOutputStream.writeObject(map);
-            objectOutputStream.close();
-
-            // Convertir le flux de sortie en tableau de bytes
-            byte[] bytes = byteArrayOutputStream.toByteArray();
-
-            // Créer une demande de téléchargement
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key("hashmap.ser")
-                    .build();
-
-            // Télécharger l'objet sérialisé sur S3
-            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(bytes));
-
-            System.out.println("HashMap sérialisée téléchargée avec succès sur S3.");
-        } catch (S3Exception e) {
-            System.err.println("Erreur S3 : " + e.awsErrorDetails().errorMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("Erreur d'E/S lors de la sérialisation de l'objet.");
-            e.printStackTrace();
-        }
-    }
-}
--- ConsolidateWorkersLambda/src/main/java/S3EventHandler.java
+++ ConsolidateWorkersLambda/src/main/java/S3EventHandler.java
@@ -1,99 +1,67 @@
 
-import java.util.List;
-
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
-import software.amazon.awssdk.services.sqs.model.Message;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 
-public class S3EventHandler {
+public class S3EventHandler implements RequestHandler<SQSEvent, String> {
 
-    public void handleRequest(SQSEvent event, Context context) {
-        Region region = Region.US_EAST_1;
-        String queueURL = "https://sqs.us-east-1.amazonaws.com/116404946400/sqs-summarize-worker-021095";
-        SqsClient sqsClient = SqsClient.builder().region(region).build();
+    private final S3Client s3Client;
+    private final String outputBucket = "consolidate-worker-lambda-021095";
 
-        ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
-                .queueUrl(queueURL)
-                .maxNumberOfMessages(1)
-                .build();
+    public S3EventHandler() {
+        this.s3Client = S3Client.builder().build();
+    }
 
-        List<Message> messages = sqsClient.receiveMessage(receiveRequest).messages();
-
-        if (!messages.isEmpty()) {
-            Message msg = messages.get(0);
+    @Override
+    public String handleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage message : event.getRecords()) {
             try {
-                // Parse the JSON
+                // Parse the JSON message
                 ObjectMapper mapper = new ObjectMapper();
-                JsonNode jsonNode = mapper.readTree(msg.body());
+                JsonNode jsonNode = mapper.readTree(message.getBody());
 
-                // Extract bucket name and file name from the JSON structure
+                // Extract bucket name and file name from the S3 event
                 String bucketName = jsonNode.get("Records").get(0)
                         .get("s3").get("bucket").get("name").asText();
                 String fileName = jsonNode.get("Records").get(0)
                         .get("s3").get("object").get("key").asText();
 
-                S3Client s3 = S3Client.builder().region(region).build();
-
-                // Check file exists
-                ListObjectsRequest listObjects = ListObjectsRequest.builder()
+                // Process the file
+                GetObjectRequest objectRequest = GetObjectRequest.builder()
+                        .key(fileName)
                         .bucket(bucketName)
                         .build();
-                ListObjectsResponse res = s3.listObjects(listObjects);
-                List<S3Object> objects = res.contents();
 
-                if (objects.stream().anyMatch((S3Object x) -> x.key().equals(fileName))) {
-                    // Retrieve file
-                    GetObjectRequest objectRequest = GetObjectRequest.builder()
-                            .key(fileName)
-                            .bucket(bucketName)
-                            .build();
+                ConsolidateWorker worker = new ConsolidateWorker(outputBucket);
+                worker.run(objectRequest);
 
-                    ConsolidateWorker worker = new ConsolidateWorker("consolidate-worker-lambda-021095");
-                    worker.run(objectRequest);
+                // Delete the processed file
+                deleteS3Object(bucketName, fileName);
 
-                    // Delete the message
-                    DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
-                            .queueUrl(queueURL)
-                            .receiptHandle(msg.receiptHandle())
-                            .build();
-                    sqsClient.deleteMessage(deleteMessageRequest);
-
-                    // Delete the file
-                    try {
-                        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                                .bucket(bucketName)
-                                .key(fileName)
-                                .build();
-                        s3.deleteObject(deleteObjectRequest);
-                    } catch (S3Exception e) {
-                        System.err.println("S3 Error: " + e.awsErrorDetails().errorMessage());
-                        e.printStackTrace();
-                    }
-                } else {
-                    System.out.println("File is not available in the Bucket");
-                }
-
-            } catch (JsonProcessingException e) {
-                System.err.println("Error parsing JSON: " + e.getMessage());
-                e.printStackTrace();
+            } catch (Exception e) {
+                context.getLogger().log("Error processing message: " + e.getMessage());
+                throw new RuntimeException("Error processing SQS message", e);
             }
-        } else {
-            System.out.println("Queue is empty");
+        }
+        return "Processing completed";
+    }
+
+    private void deleteS3Object(String bucketName, String key) {
+        try {
+            DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(deleteRequest);
+        } catch (S3Exception e) {
+            throw new RuntimeException("Error deleting S3 object: " + e.getMessage(), e);
         }
     }
 }
